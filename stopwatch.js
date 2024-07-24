feea2220789ba89a46a583f63a7c76b4fce29c59
padmapriya-debug <padma7priya7@gmail.com>
2024-07-24 13:55:58 +0530

    stopwatch_project uploading

--- stopwatch.js
+++ stopwatch.js
@@ -1,19 +1,35 @@
-document.getElementById(`00 hrs : 00 mins : 00 secs`)
-function sw(){
-let hrs=0;
-let mins=0;
-let secs=0;
+let mints = 0;
+let sec = 0;
+let hrs = 0;
+let intervalId; 
 
-sec++;
-if(sec==60){
-    min++;
-    secs=0;
-    
-    if(mins==60){
-        hrs++;
-        mins=0;
-    }
+function startime() {
+  let block = document.querySelector(".time");
+
+  if (sec == 59) {
+    mints++;
+    sec = 0;
+  }
+  if (mints == 60) {
+    hrs++;
+    mints = 0;
+  }
+  sec++;
+  block.innerText = `${hrs} : ${mints} : ${sec}`;
 }
-document.getElementById("table")=`${hrs}:${mins}:${secs}`
-document
+
+function start() {
+  if (!intervalId) {
+    intervalId = setInterval(startime, 1000); // Start the interval and store its ID
+  }
 }
+
+function stop() {
+  clearInterval(intervalId); 
+  intervalId = null; 
+}
+
+function reset() {
+location.reload()
+}
+
